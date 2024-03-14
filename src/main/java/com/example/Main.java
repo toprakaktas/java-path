677bfce7e0ec43aa1bf181c361fca50ef4f16604
Toprak <topraktas19@hotmail.com>
2024-03-15 02:16:40 +0300

    Starting on Java journey.

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, this repository is for my java improvement path!");
+    }
+}
