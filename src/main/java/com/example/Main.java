6f97e1b37338ccf7b06d75c616af5fc29b154ff2
Toprak <topraktas19@hotmail.com>
2024-03-15 03:26:21 +0300

    Using existing interfaces.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.Random;
+import java.util.function.IntBinaryOperator;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,5 +28,12 @@ public class Main {
         };
         System.out.println(calculator.calculate(4,7));
 
+        // Instead of using the calculator interface that I created, I can use ready-made one.
+        IntBinaryOperator calculator2 = (int x, int y) -> {
+            Random random = new Random();
+            int randomNumber = random.nextInt(50);
+            return x * y + randomNumber;
+        };
+        System.out.println(calculator2.applyAsInt(3,5));
     }
 }
